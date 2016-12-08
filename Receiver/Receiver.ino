35430d3a2beb0f9f4ae70280a302b83cbda34348
Sandy Suh <asuh@college.harvard.edu>
2016-12-07 20:36:29 -0500

    receiver shows health

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -11,6 +11,11 @@
 int RECV_PIN = 11;
 
 int health = 8;
+int latchPin = 8;
+int clockPin = 9;
+int dataPinRed = 0;
+int dataPinBlue = 1;
+int dataPinGreen = 2;
 
 IRrecv irrecv(RECV_PIN);
 
@@ -18,6 +23,12 @@ decode_results results;
 
 void setup()
 {
+  //set pins to output so you can control the shift register
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPinRed, OUTPUT);
+  pinMode(dataPinGreen, OUTPUT);
+  pinMode(dataPinBlue, OUTPUT);
   Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
 }
@@ -28,7 +39,30 @@ void loop() {
       --health;
     }
     Serial.println(health);
+    displayHealth(health);
     irrecv.resume(); // Receive the next value
   }
   delay(100);
+}
+
+void displayHealth(int hp) {
+    int numberToDisplay = 0.5 + pow(2,8-hp)-1;
+  //for (int numberToDisplay = 0; numberToDisplay < pow(2,8); numberToDisplay++) {
+  
+    // take the latchPin low so 
+    // the LEDs don't change while you're sending in bits:
+    digitalWrite(latchPin, LOW);
+
+
+    // shift out the high byte first:
+    //shiftOut(dataPin, clockPin, MSBFIRST, (numberToDisplay>>8));  // most significant bit (MSB) goes out first;
+
+   // shift out the low byte next:
+    shiftOut(dataPinRed, clockPin, MSBFIRST, numberToDisplay);  // most significant bit (MSB) goes out first;
+    shiftOut(dataPinGreen, clockPin, MSBFIRST, numberToDisplay);  // most significant bit (MSB) goes out first;
+
+    digitalWrite(latchPin, HIGH);
+ 
+    // pause before next value:
+    delay(500);
 }
